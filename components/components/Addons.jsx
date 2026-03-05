fbb6c6236e1c318125b335aae61f4632e0cd160b
Anshumantyagi2004 <161432160+Anshumantyagi2004@users.noreply.github.com>
2026-03-05 13:05:25 +0530

    spelling fix

--- components/components/Addons.jsx
+++ components/components/Addons.jsx
@@ -14,12 +14,12 @@ const Addons = () => {
 
           <div className="flex flex-col items-center gap-1">
             <img src="/image/addons/gem.png" className="h-22 w-35" />
-            <p className="font-semibold text-gray-900 text-xl">GEM Aprroved Products</p>
+            <p className="font-semibold text-gray-900 text-xl">GEM Approved Products</p>
           </div>
 
           <div className="flex flex-col items-center gap-2">
             <img src="/certificate (1).webp" className="h-18 w-18" />
-            <p className="font-semibold text-gray-900 text-xl">All Necessary Certificate</p>
+            <p className="font-semibold text-gray-900 text-xl">All Necessary Certificates</p>
           </div>
 
           <div className="flex flex-col items-center gap-2">
@@ -29,7 +29,7 @@ const Addons = () => {
 
           <div className="hidden md:flex flex-col items-center gap-2">
             <img src="/meeting.webp" className="h-18 w-18" />
-            <p className="font-semibold text-gray-900 text-xl">Experince Certificate for Tender</p>
+            <p className="font-semibold text-gray-900 text-xl">Experience Certificate for Tender</p>
           </div>
         </div>
       </div>
